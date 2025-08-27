dea9efad303e40e0ef072520c0b32bb605590716
AlexKT1022 <alexkt1022@gmail.com>
2025-08-27 11:45:31 -0400

    formatting

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-import express from "express";
+import express from 'express';
 const app = express();
 export default app;
 
-import employees from "#db/employees";
+import employees from '#db/employees';
 
-app.route("/").get((req, res) => {
-  res.send("Hello employees!");
+app.route('/').get((req, res) => {
+  res.send('Hello employees!');
 });
 
-app.route("/employees").get((req, res) => {
+app.route('/employees').get((req, res) => {
   res.send(employees);
 });
 
 // Note: this middleware has to come first! Otherwise, Express will treat
 // "random" as the argument to the `id` parameter of /employees/:id.
-app.route("/employees/random").get((req, res) => {
+app.route('/employees/random').get((req, res) => {
   const randomIndex = Math.floor(Math.random() * employees.length);
   res.send(employees[randomIndex]);
 });
 
-app.route("/employees/:id").get((req, res) => {
+app.route('/employees/:id').get((req, res) => {
   const { id } = req.params;
 
   // req.params are always strings, so we need to convert `id` into a number
@@ -27,7 +27,7 @@ app.route("/employees/:id").get((req, res) => {
   const employee = employees.find((e) => e.id === +id);
 
   if (!employee) {
-    return res.status(404).send("Employee not found");
+    return res.status(404).send('Employee not found');
   }
 
   res.send(employee);
