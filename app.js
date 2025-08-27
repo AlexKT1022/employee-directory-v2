e0ffdee524c18c476b9072454a710458d0b18c65
AlexKT1022 <alexkt1022@gmail.com>
2025-08-27 11:48:17 -0400

    refactored routes

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 import express from 'express';
-const app = express();
-export default app;
-
 import employees from '#db/employees';
 
-app.route('/').get((req, res) => {
+const app = express();
+
+app.get('/', (req, res) => {
   res.send('Hello employees!');
 });
 
-app.route('/employees').get((req, res) => {
+app.get('/employees', (req, res) => {
   res.send(employees);
 });
 
 // Note: this middleware has to come first! Otherwise, Express will treat
 // "random" as the argument to the `id` parameter of /employees/:id.
-app.route('/employees/random').get((req, res) => {
+app.get('/employees/random', (req, res) => {
   const randomIndex = Math.floor(Math.random() * employees.length);
+
   res.send(employees[randomIndex]);
 });
 
-app.route('/employees/:id').get((req, res) => {
+app.get('/employees/:id', (req, res) => {
   const { id } = req.params;
 
   // req.params are always strings, so we need to convert `id` into a number
@@ -32,3 +32,5 @@ app.route('/employees/:id').get((req, res) => {
 
   res.send(employee);
 });
+
+export default app;
